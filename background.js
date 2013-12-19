f409a6fb487f67be48d725f2affe56f861a54df2
Kyle <Kylemit@gmail.com>
2013-12-19 08:34:21 -0500

    Fixed Browser Action Toggle
    The browser action poorly managed state previously by just setting a false variable and toggeling, but sometimes the js execution environment would reset and the button icon would be off by one.  There's no way to check the state of the button explicitly.  Instead, the program now toggles the icon text between 'Link Finder' (default) and 'Clear Links' (once clicked).  It can use this by checking the title state and set the button appropriately.

--- background.js
+++ background.js
@@ -1,17 +1,23 @@
-var tooltipsVisible = false;
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript({file: "jquery.min.js"}, function(){
-		chrome.tabs.executeScript({file: "bootstrap.min.js"}, function(){
-			chrome.tabs.insertCSS({file: "bootstrap.min.css"}, function(){
-				chrome.tabs.executeScript({file: "script.js"}, function(){
-					var image = tooltipsVisible ?
-							"Icons/LogoSmall19.png" :
-							"Icons/LogoSmallEmpty19.png";
-					tooltipsVisible = !tooltipsVisible;
-					chrome.browserAction.setIcon({path: image, tabId: tab.id});
-				});
-			});
-		});
-	});
+    chrome.tabs.executeScript({file: "jquery.min.js"}, function(){
+        chrome.tabs.executeScript({file: "bootstrap.min.js"}, function(){
+            chrome.tabs.insertCSS({file: "bootstrap.min.css"}, function(){
+                chrome.tabs.executeScript({file: "script.js"}, function(){
+                    chrome.browserAction.getTitle({tabId: tab.id}, function (tabTitle){
+                        var initalState = (tabTitle === "Link Finder");
+                        var newImage = initalState ?
+                                        "Icons/LogoSmall19.png" :
+                                        "Icons/LogoSmallEmpty19.png";
+                        var newTitle = initalState ?
+                                        "Clear Links" :
+                                        "Link Finder";
+                        chrome.browserAction.setIcon({path: newImage, tabId: tab.id});
+                        chrome.browserAction.setTitle({title: newTitle, tabId: tab.id});
+                    });
+
+                });
+            });
+        });
+    });
 });
